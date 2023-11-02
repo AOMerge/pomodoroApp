7ed642cd4197c458c2a8fc6afee2f42d5488c591
Ángel Ortega <codenode.ortega@gmail.com>
2023-11-02 01:37:23 -0600

    Enhance UI with audio and timing features
    - Add sound effect to the button.
    - Integrate a stopwatch/timer into the application.

--- App.js
+++ App.js
@@ -1,21 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { styles, Colors } from './src/style/style'
-import { StyleSheet, Text, View, Button, SafeAreaView, Platform } from 'react-native';
-import { useState } from 'react';
-import Timer from './src/components/timer.component';
-import Header from './src/components/header.component';
+import { StatusBar } from "expo-status-bar";
+import { styles, Colors } from "./src/style/style";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  SafeAreaView,
+  Platform,
+} from "react-native";
+import { useState, useEffect } from "react";
+import Timer from "./src/components/timer.component";
+import Header from "./src/components/header.component";
+import { Audio } from "expo-av";
 
 export default function App() {
   const [isWorking, setisWorking] = useState(false);
-  const [time , setTime] = useState(25*60);
-  const [currentTime, setCurrentTime] = useState("POMO"|"SHORT"|"LONG");
+  const [time, setTime] = useState(25 * 60);
+  const [currentTime, setCurrentTime] = useState("POMO" | "SHORT" | "LONG");
   const exchangebgColor = [Colors.A300, Colors.A200, Colors.tersiary];
   const [isActive, setIsActive] = useState(false);
 
+  useEffect(()=>{
+    let interval = null;
+    if(isActive){
+      interval = setInterval(()=>{
+        setTime(time => time - 1);
+      }, 1000);
+    }else if(!isActive && time !== 0){
+      clearInterval(interval);
+    }
+    return ()=> clearInterval(interval);
+  }, [isActive, time]);
+
   const handleStart = () => {
+    playShound();
     setIsActive(!isActive);
   };
-  
+  const playShound = async () => {
+    const { sound } = await Audio.Sound.createAsync(
+      require("./assets/413690_7950035-lq.mp3")
+    );
+    await sound.playAsync();
+  };
+
   return (
     <SafeAreaView
       style={[
@@ -29,7 +56,10 @@ export default function App() {
         setCurrentTimer={setCurrentTime}
       />
       <Timer time={time} />
-      <Button onPress={() => handleStart()} title={ isActive ? "end" : "start"} />
+      <Button
+        onPress={() => handleStart()}
+        title={isActive ? "end" : "start"}
+      />
       <StatusBar style="auto" />
     </SafeAreaView>
   );
